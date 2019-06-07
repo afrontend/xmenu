c31323ae596ecbf9f356ed00f82bb18cd1822b88
afrontend <frontend.wordpress@gmail.com>
2019-06-07 23:55:29 +0900

    process arguments of app

---
+++ src/cli.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+const Configstore = require('configstore');
+const _ = require('lodash');
+const colors = require('colors');
+const figlet = require('figlet');
+const fp = require('lodash/fp');
+const inquirer = require('inquirer');
+const program = require('commander');
+const pkg = require('../package.json');
+
+const conf = new Configstore(pkg.name, {});
+const getDefaultCmd = () => (conf.get('cmd') === undefined ? 'javascript' : conf.get('cmd'));
+const setDefaultCmd = (cmd) => (conf.set('cmd', cmd));
+
+const introMessage = (msg) => {
+  console.log(figlet.textSync(msg, {
+    font: 'Standard',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+  }));
+};
+
+program
+  .version(pkg.version)
+  .option('-i, --interactive', 'interactive mode')
+  .option('-t, --cmd [cmd]', 'cmd')
+  .parse(process.argv);
+
+function activate(option) {
+  if (option.interactive) {
+    introMessage('xmenu');
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'cmd',
+          message: '명령어',
+          default: getDefaultCmd()
+        }
+      ])
+      .then(answers => {
+        setDefaultCmd(answers['cmd']);
+      });
+  }
+
+}
+
+activate(program);
