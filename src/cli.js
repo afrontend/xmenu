2b06636875130f4299ac63b67696df2cbe44ec13
afrontend <frontend.wordpress@gmail.com>
2019-06-08 20:02:55 +0900

    lint

--- src/cli.js
+++ src/cli.js
@@ -1,48 +1,47 @@
 #!/usr/bin/env node
-const Configstore = require('configstore');
-const _ = require('lodash');
-const colors = require('colors');
-const figlet = require('figlet');
-const fp = require('lodash/fp');
-const inquirer = require('inquirer');
-const program = require('commander');
-const pkg = require('../package.json');
+const Configstore = require("configstore");
+const figlet = require("figlet");
+const inquirer = require("inquirer");
+const pkg = require("../package.json");
+const program = require("commander");
 
 const conf = new Configstore(pkg.name, {});
-const getDefaultCmd = () => (conf.get('cmd') === undefined ? 'javascript' : conf.get('cmd'));
-const setDefaultCmd = (cmd) => (conf.set('cmd', cmd));
+const getDefaultCmd = () =>
+  conf.get("cmd") === undefined ? "javascript" : conf.get("cmd");
+const setDefaultCmd = cmd => conf.set("cmd", cmd);
 
-const introMessage = (msg) => {
-  console.log(figlet.textSync(msg, {
-    font: 'Standard',
-    horizontalLayout: 'default',
-    verticalLayout: 'default'
-  }));
+const introMessage = msg => {
+  console.log(
+    figlet.textSync(msg, {
+      font: "Standard",
+      horizontalLayout: "default",
+      verticalLayout: "default"
+    })
+  );
 };
 
 program
   .version(pkg.version)
-  .option('-i, --interactive', 'interactive mode')
-  .option('-t, --cmd [cmd]', 'cmd')
+  .option("-i, --interactive", "interactive mode")
+  .option("-t, --cmd [cmd]", "cmd")
   .parse(process.argv);
 
 function activate(option) {
   if (option.interactive) {
-    introMessage('xmenu');
+    introMessage("xmenu");
     inquirer
       .prompt([
         {
-          type: 'input',
-          name: 'cmd',
-          message: '명령어',
+          type: "input",
+          name: "cmd",
+          message: "명령어",
           default: getDefaultCmd()
         }
       ])
       .then(answers => {
-        setDefaultCmd(answers['cmd']);
+        setDefaultCmd(answers.cmd);
       });
   }
-
 }
 
 activate(program);
